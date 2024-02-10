4ccf3f8921c7f1cc3fa241191eaafe6298fa0dce
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2024-02-10 02:50:01 +0100

    Demonstrate morph problem with turbo frames

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Post.find_or_create_by!(title: "Post 1")
+Post.find_or_create_by!(title: "Post 1")
